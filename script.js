3fb8890e2b7756d7b444228d32f6b17c50399886
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-11-22 12:49:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+var functionName = function() {
+  alert("functionName");
+};
+
+// Test the function
+functionName();
+var functionName = function() {
+  alert(arguments.callee.name);
+};
+
+// Test the function
+functionName();
